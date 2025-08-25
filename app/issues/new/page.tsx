742717aac0a0ab7267d6dc987dbe91151e89ecc9
skeshavthalwal155 <keshavthalwal155@gmail.com>
2025-08-25 18:17:57 +0530

    Extract Error Message Component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod'
 import { createIssueSchema } from '@/app/validationSchemas';
 import z from 'zod'
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -34,18 +35,23 @@ const NewIssuePage = () => {
           } catch (error) {
             setError('An unexpected error occurred.')
           }
-
         })}>
 
         <TextField.Root placeholder='Title' {...register('title')} />
-        {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+        <ErrorMessage>
+          {errors.title?.message}
+        </ErrorMessage>
+
+
         <Controller
           name='description'
           control={control}
           render={({ field }) => <SimpleMDE placeholder='Description' {...field} />
           }
         />
-        {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+        <ErrorMessage>
+          {errors.description?.message}
+        </ErrorMessage>
         <Button>Submit New Issue</Button>
       </form>
     </div>
