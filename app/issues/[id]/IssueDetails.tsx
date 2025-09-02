f79116d3196a6bd66b0b48cd3d34654504ed509b
skeshavthalwal155 <keshavthalwal155@gmail.com>
2025-09-02 21:19:55 +0530

    Add a Delete Button

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -12,7 +12,7 @@ const IssueDetails = ({ issue }: { issue: Issues }) => {
                 <IssueStatusBadge status={issue.status} />
                 <Text>{issue.createdAt.toDateString()}</Text>
             </Flex>
-            <Card className='prose' mt={'4'}>
+            <Card className=' max-w-full' mt={'4'}>
                 <ReactMarkdown>{issue.description}</ReactMarkdown>
             </Card>
         </>
