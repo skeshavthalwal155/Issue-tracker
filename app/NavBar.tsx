5428ed70b9ee9c5166583985c7bfb47feb4f313b
skeshavthalwal155 <keshavthalwal155@gmail.com>
2025-09-04 18:29:47 +0530

    Add the Login and Logout Links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,8 +5,12 @@ import { usePathname } from 'next/navigation';
 import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
 import classnames from 'classnames';
+import { useSession } from 'next-auth/react'
+import { Box } from '@radix-ui/themes';
+
 const NavBar = () => {
     const currentPath = usePathname();
+    const { status, data: session } = useSession()
 
     const links = [
         { label: 'Dashboard', href: '/' },
@@ -18,17 +22,26 @@ const NavBar = () => {
             <Link href={'/'}><AiFillBug /></Link>
             <ul className='flex space-x-6'>
                 {links.map(link =>
-                    <Link
-                        key={link.href}
-                        className={classnames({
-                            'text-zinc-900': link.href === currentPath,
-                            "text-zinc-500": link.href !== currentPath,
-                            "hover:text-zinc-800 transition-colors": true
-                        })}
-                        href={link.href}>{link.label}
-                    </Link>
+                    <li key={link.href}>
+                        <Link
+                            className={classnames({
+                                'text-zinc-900': link.href === currentPath,
+                                "text-zinc-500": link.href !== currentPath,
+                                "hover:text-zinc-800 transition-colors": true
+                            })}
+                            href={link.href}>{link.label}
+                        </Link>
+                    </li>
                 )}
             </ul>
+            <Box>
+                {status === "authenticated" && (
+                    <Link href={'/api/auth/signout'}>Logout</Link>
+                )}
+                {status === "unauthenticated" && (
+                    <Link href={'/api/auth/signin'}>Login</Link>
+                )}
+            </Box>
         </nav >
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import NavBar from './NavBar';
 import './theme-config.css';
+import AuthProvider from './auth/Provider';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,15 +26,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="violet">
-          <NavBar />
-          <main className='p-5'>
-            <Container>
-              {children}
-            </Container>
-          </main>
-          {/* <ThemePanel /> */}
-        </Theme>
+        <AuthProvider>
+          <Theme accentColor="violet">
+            <NavBar />
+            <main className='p-5'>
+              <Container>
+                {children}
+              </Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   )
